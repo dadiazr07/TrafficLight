9da79b434285836ac784fa48dcefc2f897e716a0
dadiazr07 <dadiazr.07@gmail.com>
2023-02-03 23:47:55 +0000

    useState done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
+	const [colorSelect, setColorSelect] = useState("")
 	return (
 		<>
-			<div className="todo w-100 container-fluid bg-secondary  justify-content-center">
+			<div className="todo w-100 container-fluid bg-secondary justify-content-center">
 				<div className="semaforo container col p-5 bg-black rounded-5">
-					<div className="luz bg-danger rounded-circle selected">
+					<div onClick={() => setColorSelect("bg-danger")} 
+					className={"luz bg-danger rounded-circle " + (colorSelect === "bg-danger" ? " selectedRed" : "")}>
 					</div>
-					<div className="luz bg-warning rounded-circle">
+
+					<div onClick={() => setColorSelect("bg-warning")} 
+					className={"luz bg-warning rounded-circle " + (colorSelect === "bg-warning" ? " selectedYellow" : "")}>
 					</div>
-					<div className="luz bg-success rounded-circle">
+
+					<div onClick={() => setColorSelect("bg-success")} 
+					className={"luz bg-success rounded-circle " + (colorSelect === "bg-success" ? " selectedGreen" : "")}>
 					</div>
 				</div>
 			</div>
